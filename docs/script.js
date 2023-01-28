98a97dc984480eecb0a432dfd7cc5a43fc2d79e2
DivineDomokuma10 <divinedomokuma@gmail.com>
2023-01-29 00:49:27 +0100

    'tired'

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,3 @@
-import rowDice from "./modules/rowDice.js";
-import stopRowAndEvalWin from "./modules/stopRowAndEvalWin.js";
 
 const dices = [
     './dice/dice0.jpg',
@@ -18,15 +16,56 @@ const diceCon = document.querySelector('.dice-con');
 
 const btn = document.querySelector('.play-btn');
 
+
+
+
+const rowDice = (canRow,rowParent) => {
+    const rowElems = rowParent.children;
+    if(canRow){
+        Array.from(rowElems).forEach(rowElem =>{
+            rowElem.style.animationName = 'rowing-dice';
+        })
+    }
+}
+
+const stopRowAndEvalWin = (diceCon,title,diceArr) => {
+    const rowingDices = diceCon.children;
+    setTimeout(() => Array.from(rowingDices).forEach(rowingDice => {
+        evalWin(title,diceArr,diceCon,rowingDice)
+    }), 1000);
+
+}
+
+const evalWin = (t,diceArr,diceCon,rowingDice) => {
+    const dice = diceCon.children;
+    const random = ()=> Math.floor( Math.random() * (diceArr.length - 1))
+    let ply1 = random();
+    let ply2 = random();
+
+    if (ply1 > ply2) {
+        dice[0].src = diceArr[ply1];
+        dice[1].src = diceArr[ply2];
+        rowingDice.style.animationName = '';
+        t.textContent = 'Player 1 wins!'
+    }
+    else if (ply2 > ply1) {
+        dice[0].src = diceArr[ply1];
+        dice[1].src = diceArr[ply2];
+        t.textContent = 'Player 2 wins!'
+        rowingDice.style.animationName = '';
+    }
+    else{
+        dice[0].src = diceArr[ply1];
+        dice[1].src = diceArr[ply2];
+        t.textContent = "It's a tie!"
+        rowingDice.style.animationName = '';
+
+    }
+}
+
 const click = () => {
     rowDice(diceIsRowing,diceCon);
     stopRowAndEvalWin(diceCon,headText,dices,diceCon);  
 };
 
 btn.addEventListener("click",click);
-
-const oneDecPLace = val =>{ 
-    let str = val.toString();
-    let oneDecPlc = str.slice(0,3);
-    return parseFloat(oneDecPlc);
-}
